f74000c4c463f3f20947ff77655fbaf5d796224a
Nipun Ramani <ramaninipun@gmail.com>
2020-09-11 15:19:07 +0530

    added simple gray scale

--- AVXIsNotFun/AVXIsNotFun.cpp
+++ AVXIsNotFun/AVXIsNotFun.cpp
@@ -15,6 +15,9 @@ using namespace std;
 float a[256] = { 0 };
 float b[256] = { 0 };
 float c[256] = { 0 };
+int width = 0;
+int height = 0;
+unsigned char* img_buffer = nullptr;
 static void simd(benchmark::State& state) {
 	for (auto _ : state) {
 		__m256 result, B, C;
@@ -33,6 +36,30 @@ static void nonsimd(benchmark::State& state){
 		}
 	}
 }
+void _toGreyScale(unsigned char* rgb_img, int width, int height, int components, unsigned char * output) {
+	
+	unsigned int out_idx = 0;
+	for (unsigned int i = 0; i < width * height * components; i += 4) {
+		unsigned char r = rgb_img[i];
+		unsigned char g = rgb_img[i + 1];
+		unsigned char b = rgb_img[i + 2];
+		int out = (r + g + b) / 3;
+		output[out_idx] = out;
+		output[out_idx + 1] = out;
+		output[out_idx + 2] = out;
+		output[out_idx + 3] = 255;
+		out_idx += 4;
+	}
+}
+static void BM_toGreyScale(benchmark::State& state) {
+	int components = 4;
+	unsigned char* output = new unsigned char[width * height * components];
+	
+	for (auto _ : state) {
+		_toGreyScale(img_buffer, width, height, components, output);
+	}
+	delete output;
+}
 void fill_arrays() {
 	for (int i = 0; i < 256; i++) {
 		b[i] = 1.0;
@@ -40,8 +67,6 @@ void fill_arrays() {
 
 	}
 }
-BENCHMARK(nonsimd);
-BENCHMARK(simd);
 
 int google_benchmark(int argc, char** argv) {
 	benchmark::Initialize(&argc, argv);
@@ -61,29 +86,41 @@ int check_arrays() {
 	}
 	return ret;
 }
-
+BENCHMARK(BM_toGreyScale)->Unit(benchmark::TimeUnit::kMicrosecond);
+//unsigned char* toGreyScale(unsigned char* rgb_img, int width, int height,int components) {
+//	unsigned char * output = new unsigned char[width * height * components];
+//	unsigned int out_idx = 0;
+//	for (unsigned int i = 0; i < width*height*components; i+=4) {
+//		unsigned char r = rgb_img[i];
+//		unsigned char g = rgb_img[i+1];
+//		unsigned char b = rgb_img[i+2];
+//		int out = (r + g + b) / 3;
+//		output[out_idx] = out;
+//		output[out_idx+1] = out;
+//		output[out_idx+2] = out;
+//		output[out_idx + 3] = 255;
+//		out_idx += 4;
+//	}
+//	return output;
+//}
 int main(int  n, char * argv[])
 {
-	google_benchmark(n, argv);
-	if (n == 3) {
+	
 		std::cout << argv[0] << "\n";
-		std::string image_loc = argv[1];
-		std::string image_out = argv[2];
-		int width = 0;
-		int height = 0;
+		//std::string image_loc = argv[1];
+		//std::string image_out = argv[2];
+		width = 0;
+		height = 0;
 		int components = 0;
-		unsigned char* img_buffer = stbi_load(image_loc.c_str(), &width, &height, &components, 0);
-		//Filtering stuff
-		fill_arrays();
-		
-		stbi_write_jpg(image_out.c_str(), width, height, components, img_buffer, 1);
-		STBI_FREE(img_buffer);
+		img_buffer = stbi_load("./HelloKitty.jpg", &width, &height, &components, 4);
+		google_benchmark(n, argv);
+	/*	unsigned char * grey_img = toGreyScale(img_buffer, width, height,4);
+		stbi_write_jpg("./Trash.jpg", width, height, 4, grey_img, 100);
+	*/	STBI_FREE(img_buffer);
+		//delete grey_img;
 		std::cout << "Run successful.\n";
 
-	}
-	else
-	{
-		std::cout << "Usage: AVXIsNotFun.exe /path/to/input.jpeg /path/to/output.jpeg \n";
-	}
+	
+	
 	return 0;
 }
