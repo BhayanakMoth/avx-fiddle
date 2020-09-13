2272dbeb10a015c1745167c4770ecea1d971901c
Nipun Ramani <ramaninipun@gmail.com>
2020-09-13 21:46:13 +0530

    testing average across 3 vectors

--- AVXIsNotFun/AVXIsNotFun.cpp
+++ AVXIsNotFun/AVXIsNotFun.cpp
@@ -11,30 +11,40 @@
 #include <string>
 
 using namespace std;
-
-float a[256] = { 0 };
-float b[256] = { 0 };
-float c[256] = { 0 };
+const long int length = 1024*1024*8;
+int * a = nullptr;
+int * b = nullptr;
+int * c = nullptr;
+int * d = nullptr;
 int width = 0;
 int height = 0;
 unsigned char* img_buffer = nullptr;
-static void simd(benchmark::State& state) {
+int div_val[8] = { 3,3,3,3,3,3,3,3 };
+__m256i result, B, C, D, E;
+static void BM_simd(benchmark::State& state) {
+
 	for (auto _ : state) {
-		__m256 result, B, C;
-		for (int i = 0; i < 256; i += sizeof(__m256) / sizeof(float)) {
-			B = _mm256_load_ps(&b[i]);
-			C = _mm256_load_ps(&c[i]);
-			result = _mm256_add_ps(B, C);
-			_mm256_store_ps(&a[i], result);
+		
+		for (int i = 0; i < length; i += sizeof(__m256i) / sizeof(unsigned int)) {
+			B = _mm256_lddqu_si256((__m256i*)(&b[i]));
+			C = _mm256_lddqu_si256((__m256i*)(&b[i]));
+			D = _mm256_lddqu_si256((__m256i*)(&b[i]));
+			result = _mm256_add_epi32(B, C);
+			result = _mm256_add_epi32(result, D);
+			result = _mm256_div_epi32(result, E);
+			_mm256_storeu_si256((__m256i*)&a[i], result);
 		}
 	}
 }
-static void nonsimd(benchmark::State& state){
+static void BM_nosimd(benchmark::State& state){
 	for (auto _ : state) {
-		for (int i = 0; i < 256; i += 1) {
-			a[i] = b[i] + c[i];
+		for (int i = 0; i < length; i += 1) {
+			a[i] = (b[i] + c[i] + d[i])/3;
 		}
 	}
+}
+void _toGreyScale_simd(unsigned char* rgb_img, int width, int height, int components, unsigned char* output) {
+
 }
 void _toGreyScale(unsigned char* rgb_img, int width, int height, int components, unsigned char * output) {
 	
@@ -61,10 +71,10 @@ static void BM_toGreyScale(benchmark::State& state) {
 	delete output;
 }
 void fill_arrays() {
-	for (int i = 0; i < 256; i++) {
-		b[i] = 1.0;
-		c[i] = 2.0;
-
+	for (int i = 0; i < length; i++) {
+		b[i] = 1;
+		c[i] = 2;
+		d[i] = 3;
 	}
 }
 
@@ -86,7 +96,9 @@ int check_arrays() {
 	}
 	return ret;
 }
-BENCHMARK(BM_toGreyScale)->Unit(benchmark::TimeUnit::kMicrosecond);
+//BENCHMARK(BM_toGreyScale)->Unit(benchmark::TimeUnit::kMicrosecond);
+BENCHMARK(BM_simd)->Unit(benchmark::TimeUnit::kMicrosecond);
+BENCHMARK(BM_nosimd)->Unit(benchmark::TimeUnit::kMicrosecond);
 //unsigned char* toGreyScale(unsigned char* rgb_img, int width, int height,int components) {
 //	unsigned char * output = new unsigned char[width * height * components];
 //	unsigned int out_idx = 0;
@@ -106,19 +118,28 @@ BENCHMARK(BM_toGreyScale)->Unit(benchmark::TimeUnit::kMicrosecond);
 int main(int  n, char * argv[])
 {
 	
-		std::cout << argv[0] << "\n";
-		//std::string image_loc = argv[1];
-		//std::string image_out = argv[2];
-		width = 0;
-		height = 0;
-		int components = 0;
-		img_buffer = stbi_load("./HelloKitty.jpg", &width, &height, &components, 4);
-		google_benchmark(n, argv);
+		//std::cout << argv[0] << "\n";
+		////std::string image_loc = argv[1];
+		////std::string image_out = argv[2];
+		//width = 0;
+		//height = 0;
+		//int components = 0;
+		//img_buffer = stbi_load("./HelloKitty.jpg", &width, &height, &components, 4);
+		//
+	a = new int[length];
+	b = new int[length];
+	c = new int[length];
+	d = new int[length];
+
+	E = _mm256_loadu_epi32(&div_val[0]);
+	fill_arrays();
+	google_benchmark(n, argv);
 	/*	unsigned char * grey_img = toGreyScale(img_buffer, width, height,4);
 		stbi_write_jpg("./Trash.jpg", width, height, 4, grey_img, 100);
-	*/	STBI_FREE(img_buffer);
-		//delete grey_img;
-		std::cout << "Run successful.\n";
+	*/
+	//STBI_FREE(img_buffer);
+	//delete grey_img;
+	std::cout << "Run successful.\n";
 
 	
 	
