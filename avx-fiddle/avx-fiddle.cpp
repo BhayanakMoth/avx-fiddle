e2a9302bf75bc9225873d4b0e9b480b67f602f30
Nipun Ramani <ramaninipun@gmail.com>
2020-09-19 22:53:11 +0530

    fixed a small typo

--- avx-fiddle/avx-fiddle.cpp
+++ avx-fiddle/avx-fiddle.cpp
@@ -1,7 +1,7 @@
 ﻿// AVXIsNotFun.cpp
 //
 
-#include "AVXIsNotFun.h"
+#include "avx-fiddle.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include "../ThirdParty/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
